394bd5c8a7c30f0a4d7aaedba53b122e617852fd
AStrizh <abstrizhevskiy@gmail.com>
2018-10-21 10:20:55 -0400

    Initialized database creation package

---
+++ JavaProject/src/main/java/DatabaseCreator/Main.java
@@ -0,0 +1,26 @@
+package DatabaseCreator;
+
+import java.io.*;
+import java.io.IOException;
+
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
+
+/**
+ * Launches database creator.
+ *
+ * <P>Creates database from country data
+ *
+ * @author Aleksandr Strizhevskiy
+ * @version 1.0
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Hello, World!");
+    }
+
+
+}
