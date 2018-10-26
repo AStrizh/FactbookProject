bf3d3824b7f7d10ac8c860eec8a807f65e20da7a
AStrizh <abstrizhevskiy@gmail.com>
2018-10-26 18:23:22 -0400

    DatabaseRestarter refreshes the database

---
+++ JavaProject/src/main/java/DatabaseCreator/util/DatabaseRestarter.java
@@ -0,0 +1,53 @@
+package DatabaseCreator.util;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseRestarter {
+
+    //Connection conn;
+
+    public DatabaseRestarter() {
+
+        Connection conn = ConnectionManager.getInstance().getConnection();
+
+        String sqlFile = reader();
+        String[] sqlCommands = sqlFile.split(";");
+
+        for(String sql : sqlCommands)
+            executer(conn, sql);
+    }
+
+
+    private static String reader() {
+
+        String projectFolder = "C:/Users/Aleksandr/Documents/ParcingFactbook/";
+        String source = projectFolder + "CreateDatabase.sql";
+
+        try{
+            return new String(Files.readAllBytes(Paths.get(source)), StandardCharsets.UTF_8);
+        }catch (IOException e){
+            return null;
+        }
+
+    }
+
+    private static void executer(Connection conn, String sql){
+
+
+        try (
+                PreparedStatement stmt = conn.prepareStatement(sql)
+        ) {
+            System.out.println(sql);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            DBUtil.processException(e);
+        }
+    }
+}
