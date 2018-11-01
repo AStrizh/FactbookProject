9658a62e315feab583d887f4d661bd56cb80589f
AStrizh <abstrizhevskiy@gmail.com>
2018-10-31 21:09:12 -0400

    Updated to parse borders

--- JavaProject/src/main/java/DatabaseCreator/parsers/ParserMain.java
+++ JavaProject/src/main/java/DatabaseCreator/parsers/ParserMain.java
@@ -6,8 +6,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
+import DatabaseCreator.beans.Border;
 import DatabaseCreator.beans.City;
 import DatabaseCreator.beans.CountryMain;
+import DatabaseCreator.beans.Geography;
+import DatabaseCreator.tables.BorderManager;
 import DatabaseCreator.tables.CityManager;
 import DatabaseCreator.tables.CountryMainManager;
 import DatabaseCreator.util.ConnectionManager;
@@ -59,7 +62,7 @@ public class ParserMain {
                     countryCode = el.attributes().get("ccode").toUpperCase();
             }
 
-            //TODO:Check here for working code
+
             if(el.className().equals("category_data")){
 
                 switch(sectionTitle){
@@ -67,6 +70,26 @@ public class ParserMain {
                         introduction = introduction + el.text() + "\n";
                         break;
                     case "Geography":
+                        if(previous.className().contains("category")){
+                            if(previous.text().contains("border countries")){
+
+                                //TODO: Create method "cleanParentheses"
+                                //TODO: This parentheses method doesn't work
+                                //Method eats stuff in between parentheses too
+
+                                //String formated = removeParentheses(el.text());
+                                //String formated = normalize_removeTextInParentheses(el.text());
+                                //System.out.println(countryCode + "   " + formated);
+
+                                String borders = removeParentheses(el.text()).trim();
+                                ArrayList<Border> borderNations = processBorders(
+                                        borders.split(" km ,| km,| km"), countryCode);
+
+                                for(Border border : borderNations)
+                                    BorderManager.insert(border);
+                            }
+                        }
+
                         switch (previous.text()){
                             case "Geographic coordinates:":
                                 //System.out.println( "Geographic coordinates: " + el.text());
@@ -77,25 +100,26 @@ public class ParserMain {
                             case "Irrigated land:":
                                 //System.out.println("Irrigated land: " + el.text());
                                 break;
+
+
                             default:
                                 ;
+                        }
+                        break;
+                    case "People and Society":
+                        if(previous.className().contains("_light") &&
+                                previous.text().contains("Major urban areas")){
+
+                            ArrayList<City> cityBeans = processCities(el.text().split(";"), countryCode);
+                            for(City city : cityBeans)
+                                CityManager.insert(city);
                         }
                         break;
                     default:
                         ;
                 }
 
-                //TODO: Fit this under People and Society
-                if(previous.className().contains("_light") &&
-                        previous.text().contains("Major urban areas")){
 
-                    ArrayList<City> cityBeans = processCities(el.text().split(";"), countryCode);
-                    for(City city : cityBeans){
-                        if(city.getCapital() == true)
-                            //System.out.println("Capital: " + city.getCityName());
-                        CityManager.insert(city);
-                    }
-                }
 
             }
 
@@ -139,7 +163,47 @@ public class ParserMain {
     }
 
 
+    private static ArrayList<Border> processBorders(String[] borders, String countryCode){
+        ArrayList<Border> borderNations = new ArrayList<>();
 
+        for(String border : borders){
+            String[] borderData = border.trim().split("  | ");
+
+            StringBuilder sb = new StringBuilder();
+            int i = 0;
+            while (Pattern.matches("[a-zA-Z]+", Character.toString(borderData[i].charAt(0)))){
+                sb.append(" " + borderData[i]);
+                i++;
+            }
+            String borderName = sb.toString();
+
+            double borderKM = 0.0;
+            try{
+                borderKM = Double.parseDouble(borderData[i].replace(",",""));
+            }catch(NumberFormatException e) {
+                System.out.println("NumberFormatException: " + borderData[i]);
+
+            }catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Out of bounds! " + countryCode);
+            }
+
+            if(!borderNations.isEmpty() &&
+                              borderNations.get(borderNations.size()-1).getBorderCountry().equals(borderName) ){
+
+                borderNations.get(borderNations.size()-1).setBorderLength(
+                        borderNations.get(borderNations.size()-1).getBorderLength() + borderKM);
+
+            } else {
+                Border borderBean = new Border();
+                borderBean.setCountryCode(countryCode);
+                borderBean.setBorderCountry(borderName);
+                borderBean.setBorderLength(borderKM);
+                borderNations.add(borderBean);
+            }
+        }
+
+        return borderNations;
+    }
 
     private static ArrayList<City> processCities( String[] cities, String countryCode){
 
@@ -186,5 +250,43 @@ public class ParserMain {
             cityBeans.add(cityBean);
         }
         return cityBeans;
+    }
+
+//    private static String removeParentheses(String str){
+//
+//        StringBuilder br = new StringBuilder();
+//        //int flag = 0;
+//
+//        for(int i = 0; i<str.length(); i++){
+//            if(str.charAt(i) == '('){
+//                while (str.charAt(i) != ')')
+//                    i++;
+//                i++;
+//            }
+//            if(i<str.length())
+//                br.append(str.charAt(i));
+//        }
+//        return br.toString();
+//    }
+
+    private static String removeParentheses(String str){
+
+        StringBuilder sb = new StringBuilder();
+        int flag = 0;
+
+        for(int i = 0; i<str.length(); i++){
+            //char c = str.charAt(i);
+            switch(str.charAt(i)){
+                case '(':
+                    flag = 1;
+                    continue;
+                case ')':
+                    flag = 0;
+                    continue;
+            }
+            if(flag==0)
+                sb.append(str.charAt(i));
+        }
+        return sb.toString();
     }
 }
